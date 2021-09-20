d9a132b369bbf1121b64986c99c795d3b0c198f5
SananM <78546750+SananMuzaffarov@users.noreply.github.com>
2021-09-20 22:26:27 +0400

    Add files via upload

---
+++ LIGHT ON-OFF/light.js
@@ -0,0 +1,9 @@
+//First JS Project
+
+
+function openLight(){
+    document.getElementById("picture").src="pic_bulbon.gif";
+}
+function closeLight(){
+    document.getElementById("picture").src="pic_bulboff.gif";
+}
