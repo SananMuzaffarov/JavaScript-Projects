f6f1ff9823745f5a4955e3dac615abbca2dd29db
SananM <78546750+SananMuzaffarov@users.noreply.github.com>
2022-01-03 04:41:16 +0400

    Add files via upload

---
+++ BOOLEAN,IF-ELSE,SWITCH CASE-JAVASCRIPT/BOOLEAN/bool.js
@@ -0,0 +1,9 @@
+document.write(Boolean(10>9)); //TRUE
+
+let x = 0; // -0 also is FALSE
+document.write(Boolean(x)); //FALSE
+
+let y = false; // empty string, undefined variable, null are also FALSE
+document.write(Boolean(y)); //FALSE
+
+//Some Comparison elements: &&-Logical And; ||-Logical Or; !-Logical Not.
---
+++ BOOLEAN,IF-ELSE,SWITCH CASE-JAVASCRIPT/IF STATEMENTS/ifelse.js
@@ -0,0 +1,19 @@
+// EXAMPLE FOR "IF-ELSE"
+var x = 10;
+if(x>9){
+    document.write("It is true");
+}
+else{
+    document.write("It is false");
+};
+// EXAMPLE FOR "IF - ELSE IF - ELSE"
+const num = prompt("Enter number: ");
+if (num > 0) {
+    document.write("The number is POSITIVE");
+}
+else if (num == 0) {
+  document.write("The number is 0");
+}
+else {
+    document.write("The number is NEGATIVE");
+}
