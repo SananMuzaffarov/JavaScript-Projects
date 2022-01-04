e533a3911218980305d1a12dd4c3589b7ed816cf
SananM <78546750+SananMuzaffarov@users.noreply.github.com>
2022-01-05 03:17:59 +0400

    Add files via upload

---
+++ NUMBERS, MATH-JAVASCRIPT/num.js
@@ -0,0 +1,36 @@
+// EXAMPLE 1
+var num = 17;
+document.write(num);
+
+// EXAMPLE 2
+var num1 = 20;
+document.write(typeof(num));
+
+
+// EXAMPLE 3
+var num3 = "20";
+var num4 = 20;
+console.log(num3 + num4); //Its output will be 2020. Because one of them string and one of them is number.
+// var num5 = "2020"; It is string because it has double quotes.
+
+// EXAMPLE 4
+let x = "100";
+let y = "10";
+let z = x + y;   
+document.write(z);
+
+let a = "100";
+let b = "10";
+let c = a - b;   
+console.log(c)
+
+console.log(100/"10");
+console.log(100/"Apple");
+
+//----------------------------------------------MATH-----------------------------------------------------------------
+
+console.log(Math.pow(2,3)); // We can find 2^3.
+console.log(Math.log(1)); // We can find Logarithma 1.
+console.log(Math.sqrt(64)); // 64 is 8's square.
+console.log(Math.min(0, 150, 30, 20, -8, -200)); // We can find the min value in this range.
+console.log(Math.max(0, 150, 30, 20, -8, -200)); // We can find the max value in this range.
