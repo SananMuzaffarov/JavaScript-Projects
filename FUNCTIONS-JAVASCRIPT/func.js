4590d753203b9c84054013f8b8ffefc7c91f5938
SananM <78546750+SananMuzaffarov@users.noreply.github.com>
2022-01-04 04:14:27 +0400

    Add files via upload

---
+++ FUNCTIONS-JAVASCRIPT/func.js
@@ -0,0 +1,18 @@
+// EXAMPLE 1
+function sum(a,b){
+    return a + b;
+}
+console.log(sum(3,4)); // Its output will be 7.
+
+// EXAMPLE 2
+function pressHere(){
+    var x = 10;
+    var y = 20;
+    document.getElementById("demo").innerHTML = x + y;
+}
+
+// EXAMPLE 3
+function toCelsi(fahrenheit){
+    return (5/9) * (fahrenheit-32);
+}
+document.write(toCelsi(50)); // In here we found the fahrenheit from converting 50 celsius
