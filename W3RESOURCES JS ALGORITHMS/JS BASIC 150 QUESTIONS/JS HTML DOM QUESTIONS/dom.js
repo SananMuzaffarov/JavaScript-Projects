76b987676d8a4224fe52c54bbcd2e3d055f790ab
SananM <78546750+SananMuzaffarov@users.noreply.github.com>
2022-01-09 04:25:15 +0400

    Update dom.js

--- W3RESOURCES JS ALGORITHMS/JS BASIC 150 QUESTIONS/JS HTML DOM QUESTIONS/dom.js
+++ W3RESOURCES JS ALGORITHMS/JS BASIC 150 QUESTIONS/JS HTML DOM QUESTIONS/dom.js
@@ -18,3 +18,22 @@ function changeBack(){
     document.getElementById("as").style.background = "red";
     document.getElementById("sa").style.background = "yellow";
 }
+
+// QUESTION 4 - Here is a sample html file with a submit button. Write a JavaScript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.
+function getInfos(){
+ var a = document.getElementById("khzr").href;
+ var b = document.getElementById("khzr").hreflang;   
+ var c = document.getElementById("khzr").rel; 
+ var d = document.getElementById("khzr").target; 
+ var e = document.getElementById("khzr").type; 
+ document.write(a);
+}
+
+// QUESTION 5 - Write a JavaScript function to add rows to a table.
+function extraRow(){
+    var x = document.getElementById("Table").insertRow(0);
+    var y = x.insertCell(0);
+    var z = x.insertCell(1);
+    y.innerHTML="1";
+    z.innerHTML="2";
+}
