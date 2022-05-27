906d60cbdffafc3414eb2df72f9640ee4833a596
SananM <78546750+SananMuzaffarov@users.noreply.github.com>
2022-05-27 18:15:36 +0400

    Delete BOOLEAN,IF-ELSE,SWITCH CASE-JAVASCRIPT directory

--- BOOLEAN,IF-ELSE,SWITCH CASE-JAVASCRIPT/BOOLEAN/bool.js
+++
@@ -1,9 +0,0 @@
-document.write(Boolean(10>9)); //TRUE
-
-let x = 0; // -0 also is FALSE
-document.write(Boolean(x)); //FALSE
-
-let y = false; // empty string, undefined variable, null are also FALSE
-document.write(Boolean(y)); //FALSE
-
-//Some Comparison elements: &&-Logical And; ||-Logical Or; !-Logical Not.
